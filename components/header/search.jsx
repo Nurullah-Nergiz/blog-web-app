96799f21837315995de8b4d7b791273d93999c6f
Nurullah Nergiz <nurullah-nergiz@hotmail.com>
2024-05-27 21:33:03 +0300

    Header Update: search box is available

--- components/header/search.jsx
+++ components/header/search.jsx
@@ -1,26 +1,45 @@
 "use client";
+import { useState,useRef } from "react";
+import { useRouter } from "next/navigation";
 
 export const Search = () => {
-   function submit(e) {
-      console.log(e);
-   }
+   const [search, setSearch] = useState("");
+   const searchRef = useRef();
+   const router = useRouter();
+
+   const submit = (e) => {
+      e.code == "Enter"
+         ? router.push(`/search/${e.target.value.trim().replaceAll(" ", "+")}`)
+         : "";
+   };
+
    return (
-      <div className="hidden md:flex w-80 text-secondary">
-         <div className="py-2 px-3 flex items-center border border-current rounded-2xl relative">
-           <label htmlFor="search" className="bx bx-search"></label>
-           <input
-              type="search"
-              className="mx-2 bg-transparent outline-none"
-              id="search"
-              placeholder="Search..."
-              onKeyDown={submit}
-           />
-           <button className="bx bx-microphone"></button>
+      <div className="hidden md:flex w-80 text-secondary relative">
+         <div className="py-2 px-3 flex items-center gap-2 border border-current rounded-xl text-2xl">
+            <label htmlFor="search" className="bx bx-search "></label>
+            <input
+               type="text"
+               className="w-full bg-transparent text-base outline-none "
+               id="search"
+               placeholder="Search..."
+               onKeyDown={submit}
+               ref={searchRef}
+               onChange={(e) => setSearch(e.target.value)}
+               autoFocus
+            />
+            {!search ? (
+               <button className="bx bx-microphone"></button>
+            ) : (
+               <button
+                  className="bx bx-x"
+                  onClick={() => setSearch("")}></button>
+            )}
          </div>
-         <ul className="hidden h-80 pt-6 bg-white absolute -right-0 top-6 -left-0 border border-t-0 border-current rounded-b-2xl">
+         <ul className={`${!search?"hidden":"hidden"} h-80 py-2 px-3 bg-white absolute -right-0 top-8 -left-0 border border-t-0 border-current rounded-b-xl`}>
+               <hr />
             <li className="w">
-              <hr />
-               {/* <i className="bx bx-search"></i> */}
+               <i className="bx bx-search text-2xl"></i>
+               a
             </li>
          </ul>
       </div>
