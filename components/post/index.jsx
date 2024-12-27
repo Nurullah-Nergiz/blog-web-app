f507d4141fd0435b9d63cd450a4763a67b769136
Nurullah NERGİZ <nurullah.devs@gmail.com>
2024-12-27 19:41:14 +0300

    Comment out WindowVirtualizer component in Posts for debugging purposes

--- components/post/index.jsx
+++ components/post/index.jsx
@@ -5,11 +5,11 @@ import Post from "../widgets/post";
 export default function Posts({ posts = [] }) {
    return (
       <>
-         <WindowVirtualizer className="!h-full">
+         {/* <WindowVirtualizer className="!h-full"> */}
             {posts.map((post, index) => (
                <Post key={post._id} post={post} />
             ))}
-         </WindowVirtualizer>
+         {/* </WindowVirtualizer> */}
       </>
    );
 }
